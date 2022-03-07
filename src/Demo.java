6ab608124ba78e71ca2d698dd138025f35c3b8ce
swaro <swaro@Swaroop-PC>
2022-03-07 16:09:54 +0530

    f

--- src/Demo.java
+++ src/Demo.java
@@ -1,8 +1,14 @@
-
+import org.openqa.selenium.chrome.ChromeDriver;
 public class Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hi");//from Git
+		System.setProperty("webdriver.chrome.driver","./software/chromedriver.exe");
+		ChromeDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		driver.quit();
+//		driver.close();
+//		driver.manage().window().fullscreen();
+//		driver.manage().window().minimize();
 	}
 }
