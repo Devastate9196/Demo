5a3a0f51feea5c3a97365b2f4f8e14ea0e7516ab
swaro <swaro@Swaroop-PC>
2022-03-09 16:03:00 +0530

    ok

--- src/Demo.java
+++ src/Demo.java
@@ -1,12 +1,20 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.Keys;
+
 public class Demo {
 
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver","./software/chromedriver.exe");
-		ChromeDriver driver=new ChromeDriver();
+		System.setProperty("webdriver.chrome.driver", "./software/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.google.com/");
-		driver.quit();
+		WebElement r = driver.findElement(By.name("q"));
+		r.sendKeys("swaroop waingankar");
+		r.sendKeys(Keys.RETURN);
+//		driver.quit();
 //		driver.close();
 //		driver.manage().window().fullscreen();
 //		driver.manage().window().minimize();
---
+++ src/TitleAndUrl.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TitleAndUrl {
+public static void main(String[] args) {
+	System.setProperty("webdriver.chrome.driver", "./software/chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("https://www.google.com/");
+	System.out.println(driver.getCurrentUrl());//URL
+	
+	System.out.println(driver.getTitle());//TITLE
+	
+}
+}
